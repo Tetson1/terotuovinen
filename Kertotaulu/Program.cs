578e9a6666e42f5a8d8e1827941c23b92fb35994
tero.tuovinen <tee.tuovinen@gmail.com>
2018-06-06 16:21:09 +0300

    Add project files.

---
+++ Kertotaulu/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kertotaulu
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna numero välillä 1-10");
+            string input = Console.ReadLine();
+            int luku;
+            Int32.TryParse(input, out luku);
+
+            
+            try
+            {
+                int y = int.Parse(input);
+                {
+                    //if y > 0;
+
+                    //return true
+                    //        }
+                    //else false
+                }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Anna uusi kokonaisluku");
+            }
+            catch (Exception ex1)
+            {
+                Console.WriteLine("Anna uusi kokonaisluku");
+            }
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                int tulo = luku * i;
+                Console.WriteLine("{0} * {1} = {2}", luku , i, tulo);
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
